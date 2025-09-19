fc7695d82d0106fcb11b3d0b22211f8f84ffae04
Tejas Musale <tejasmusale99@gmail.com>
2025-09-19 23:55:17 +0530

    assign attribute

--- 01_Inception/App.js
+++ 01_Inception/App.js
@@ -1,5 +1,9 @@
 //  Hello world using React
-const reactHeading = React.createElement("h1", {id:"heading", xyz:"abc"}, "Hello from react");
+const reactHeading = React.createElement(
+  "h1",
+  { id: "heading", xyz: "abc" },
+  "Hello from react"
+);
 const reactRoot = ReactDOM.createRoot(document.getElementById("react-root"));
 
 reactRoot.render(reactHeading);
