e4c31114be43cc714d53ac6a53dc2cdc1bc9e10a
Tejas Musale <tejasmusale99@gmail.com>
2025-09-20 23:25:33 +0530

    nested element structure using react.createElement

--- 01_Inception/App.js
+++ 01_Inception/App.js
@@ -4,6 +4,31 @@ const reactHeading = React.createElement(
   { id: "heading", xyz: "abc" },
   "Hello from react"
 );
+
+// Nested Element Structure
+
+{
+  /* <div id="parent">
+  <div id="child">
+    <h1>Nested element struture using react.createElement</h1>
+  </div>
+</div>; */
+}
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement(
+      "h1",
+      {},
+      "Nested element struture using react.createElement"
+    )
+  )
+);
+
 const reactRoot = ReactDOM.createRoot(document.getElementById("react-root"));
 
-reactRoot.render(reactHeading);
+reactRoot.render(parent);
