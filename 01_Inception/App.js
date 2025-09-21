93c479cf6434cae5443d6ebc6359885d2f71eb89
Tejas Musale <tejasmusale99@gmail.com>
2025-09-21 12:44:02 +0530

    created nested elements with siblings

--- 01_Inception/App.js
+++ 01_Inception/App.js
@@ -15,18 +15,46 @@ const reactHeading = React.createElement(
 </div>; */
 }
 
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     React.createElement(
+//       "h1",
+//       {},
+//       "Nested element struture using react.createElement"
+//     )
+//   )
+// );
+
+// Nested Element with sibling
+
+//   <div id="parent">
+//   <div id="child1">
+//     <h1>Hello from heading 1</h1>
+//     <h1>Hello from heading 2</h1>
+//   </div>
+//   <div id="child2">
+//     <h1>Hello from heading 3</h1>
+//     <h1>Hello from heading 4</h1>
+//   </div>
+// </div>;
+
 const parent = React.createElement(
   "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement(
-      "h1",
-      {},
-      "Nested element struture using react.createElement"
-    )
-  )
+  { id: "parent" },[
+  React.createElement("div", { id: "child1" },[
+    React.createElement('h1',{}, 'Hello from heading 1'),
+    React.createElement('h1',{}, 'Hello from heading 2')
+  ]),
+    React.createElement("div", { id: "child2" },[
+    React.createElement('h1',{}, 'Hello from heading 3'),
+    React.createElement('h1',{}, 'Hello from heading 4')
+  ])
+  ]
+
 );
 
 const reactRoot = ReactDOM.createRoot(document.getElementById("react-root"));
