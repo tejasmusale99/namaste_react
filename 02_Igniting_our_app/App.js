0900bb8eb6cd46f695969a250975c952cb96fb7b
Tejas Musale <tejasmusale99@gmail.com>
2025-09-28 13:34:02 +0530

    ignited our app with parcel

--- 02_Igniting_our_app/App.js
+++ 02_Igniting_our_app/App.js
@@ -1,14 +1,25 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+// import {createRoot} from 'react-dom/client'   React 19
+
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "heading1"),
-    React.createElement("h1", {}, "heading2"),
+  React.createElement("div", { id: "child1", key: "child1" }, [
+    React.createElement("h1", {key:'h1-1'}, "heading1"),
+    React.createElement("h1", {key:'h1-2'}, "heading2"),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "heading3"),
-    React.createElement("h1", {}, "heading4"),
+  React.createElement("div", { id: "child2", key: "child2" }, [
+    React.createElement("h1", {key:'h1-3'}, "heading3"),
+    React.createElement("h1", {key:'h1-4'}, "heading4"),
   ]),
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// React 18 
 
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(parent)
+// React 19 
+
+// const container = document.getElementById('root')
+
+// const root = createRoot(container)
+// root.render(parent)
