29d8abf46e30a661a94649734bab92f499bdfc09
Tejas Musale <tejasmusale99@gmail.com>
2025-10-05 13:10:53 +0530

    functional coponent

--- 03_laying_the_foundation/App.js
+++ 03_laying_the_foundation/App.js
@@ -5,14 +5,31 @@ import ReactDOM from "react-dom/client";
 
 const heading = React.createElement("h1", {}, "Tejas Musale");
 
-// React.createElement => is nothing but (js object) => inside root.(render) converted into HTML Element 
+// React.createElement => is nothing but (js object) => inside root.(render) converted into HTML Element
 
-//using jsx 
+//react element using jsx
 
 // JSX => converted into (React.createElement) with the help of babel =>
-//  (React.createElement) is again JS object and inside root.(render) converted into HTML Element 
+//  (React.createElement) is again JS object and inside root.(render) converted into HTML Element
 
-const heading2 = (<h1>Hello Tejas</h1>);
+const heading2 = <h1>Hello Tejas</h1>;
+
+// function component with arrow function
+
+const Heading3 = () => {
+  return <h1>arrow functional component</h1>;
+};
+
+// functional component with normal function
+
+function Heading4() {
+  return (
+    <>
+      <h1>normal function component</h1>
+      <Heading3 /> {/*component composition when we write component inside another component*/}
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading2);
+root.render(<Heading4 />);
