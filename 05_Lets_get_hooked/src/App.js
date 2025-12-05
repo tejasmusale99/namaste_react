1f164c930bd6d63c3fe1fd8e58c7bc6fd69484a9
Tejas Musale <tejasmusale99@gmail.com>
2025-12-05 23:09:31 +0530

    Error in routing

--- 05_Lets_get_hooked/src/App.js
+++ 05_Lets_get_hooked/src/App.js
@@ -5,6 +5,7 @@ import Body from "./components/Body";
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 const AppLayout = () => {
   return (
@@ -19,6 +20,7 @@ const appRouter = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <Error />
   },
   {
     path: 'about',
---
+++ 05_Lets_get_hooked/src/components/Error.js
@@ -0,0 +1,15 @@
+import { useRouteError } from "react-router-dom";
+
+const Error = () => {
+    const err = useRouteError();
+    console.log(err)
+  return (
+    <div className="errElement">
+      <h1>{err.data}</h1>
+      <h1> {err.status}</h1>
+      <h1> {err.statusText}</h1> 
+    </div>
+  );
+};
+
+export default Error;
