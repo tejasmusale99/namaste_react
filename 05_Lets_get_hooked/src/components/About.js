37f783ee2763a5f91213a50e9d709f82f0cb2a58
Tejas Musale <tejasmusale99@gmail.com>
2026-01-02 22:02:11 +0530

    class components parent child relatiion lifecycle

--- 05_Lets_get_hooked/src/components/About.js
+++ 05_Lets_get_hooked/src/components/About.js
@@ -1,12 +1,42 @@
+import React from "react";
 import UserClass from "./UserClass";
 
-const About = () => {
-  return (
-    <>
-      <h1>About Page</h1>
-      <UserClass UserName={"tejas99"} Designation={"Full stack ai developer"}/>
-    </>
-  );
-};
+
+/******** Class component ********/
+
+class About extends React.Component {
+  constructor(props){
+    super(props)
+    console.log("parent Constructor");
+  }
+
+  componentDidMount(){
+    console.log("parent componentDidMount");
+  }
+  render() {
+    console.log("parent render");
+    
+    return (
+      <>
+        <h1>About Page</h1>
+        <UserClass
+          UserName={"tejas99"}
+          Designation={"Full stack ai developer"}
+        />
+      </>
+    );
+  }
+}
+
+/******* function Component *****/
+
+// const About = () => {
+//   return (
+//     <>
+//       <h1>About Page</h1>
+//       <UserClass UserName={"tejas99"} Designation={"Full stack ai developer"}/>
+//     </>
+//   );
+// };
 
 export default About;
