2a3f77332b8cfc2e08b76c49a5b4875608e3fc46
Tejas Musale <tejasmusale99@gmail.com>
2026-01-04 19:08:36 +0530

    fix the discount per restrocard issue

--- 05_Lets_get_hooked/src/components/RestaurantCard.js
+++ 05_Lets_get_hooked/src/components/RestaurantCard.js
@@ -1,7 +1,7 @@
 import {cdnUrl} from '../utils/constants.js'
 
 const RestaurantCard = (props) => {
-  // console.log(props.res.info)
+  console.log(props.res.info)
   const {name,avgRating,cuisines,costForTwo,sla,cloudinaryImageId,aggregatedDiscountInfoV3} = props.res.info
 
   return (
@@ -11,7 +11,8 @@ const RestaurantCard = (props) => {
           src={cdnUrl + cloudinaryImageId}
           alt="The Spice Hub"
         />
-        {aggregatedDiscountInfoV3?.header&& <span className="discount">{aggregatedDiscountInfoV3?.header} </span> } 
+        {/* {aggregatedDiscountInfoV3?.header && <span className="discount">{aggregatedDiscountInfoV3?.header} </span> }  */}
+        {aggregatedDiscountInfoV3?.header === "ITEMS"?" ":<span className="discount">{aggregatedDiscountInfoV3?.header} </span>}
         </div>
       <div className="card-content">
         <h3 className="restaurant-name">{name}</h3>
