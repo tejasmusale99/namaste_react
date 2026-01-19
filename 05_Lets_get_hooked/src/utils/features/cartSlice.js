7538e1d11e3d7c611396eee5a3358172e6d94bb2
Tejas Musale <tejasmusale99@gmail.com>
2026-01-19 21:18:25 +0530

    created a CartSlice

---
+++ 05_Lets_get_hooked/src/utils/features/cartSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: cart,
+  initialState: {
+    value: [],
+  },
+  reducers: {
+    addToCart: (state, action) => {
+      state.value.push(action.payload);
+    },
+    removeFromCart: (state) => {
+      state.value.pop();
+    },
+    clearCart: (state) => {
+      state.value.length === 0;
+    },
+  },
+});
+
+export const { addToCart, removeFromCart, clearCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
--- 05_Lets_get_hooked/src/utils/store/appStore.js
+++ 05_Lets_get_hooked/src/utils/store/appStore.js
@@ -1,7 +1,9 @@
-import {configureStore} from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 
 const appStore = configureStore({
-    
+    reducer:{
+
+    }
 })
 
 export default appStore;
