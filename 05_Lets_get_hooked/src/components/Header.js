220ce73278ec298b2bdc47004def3805370398fb
Tejas Musale <tejasmusale99@gmail.com>
2026-02-02 22:02:05 +0530

    working  on testing problem

--- 05_Lets_get_hooked/src/components/Header.js
+++ 05_Lets_get_hooked/src/components/Header.js
@@ -1,28 +1,35 @@
 import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
-import logoImage from "url:/SwaadpointLogo.png";
+// import logoImage from "url:/SwaadpointLogo.png";
+const logoUrl = new URL("../assets/SwaadpointLogo.png", import.meta.url).href;
+// import logoImage from "../assets/SwaadpointLogo.png";
 import useOnlineOffline from "../utils/customHooks/useOnlineOffline";
 import UserContext from "../utils/context/UserContext";
-import { useSelector } from 'react-redux'
+import { useSelector } from "react-redux";
+
+console.log(logoUrl);
 
 const Header = () => {
   const [isLogIn, setIsLogIn] = useState(true);
 
   const onlineStatus = useOnlineOffline();
 
-  const {loggedInUser} = useContext(UserContext)
+  const { loggedInUser } = useContext(UserContext);
 
   // console.log(loggedInUser);
-  
-const cartItems = useSelector((store)=>store.cart.items)
 
-console.log(cartItems)
+  const cartItems = useSelector((store) => store.cart.items);
+
+  console.log(cartItems);
 
   return (
     <header className="header">
       <div className="logo-container">
         <Link to="http://localhost:1234">
-          <img src={logoImage} alt="Logo" className="logo" />
+          <img src={logoUrl} alt="Logo" className="logo" />
+        </Link>
+        <Link to="/">
+          <img src="/SwaadpointLogo.png" alt="Logo" className="logo" />
         </Link>
       </div>
       <nav className="nav">
@@ -35,18 +42,19 @@ console.log(cartItems)
         <Link to="/" className="cart">
           {onlineStatus ? "Online: 🟢" : "Offline: 🔴"}
         </Link>
-        
       </nav>
       <div className="flex gap-5">
-      <button
-      className="cursor-pointer"
-        onClick={() => {
-          setIsLogIn(!isLogIn);
-        }}
-      >
-        {isLogIn ? "Log In" : "Log Out"}
-      </button>
-      <div><Link to="/about">👤 {loggedInUser}</Link></div>
+        <button
+          className="cursor-pointer"
+          onClick={() => {
+            setIsLogIn(!isLogIn);
+          }}
+        >
+          {isLogIn ? "Log In" : "Log Out"}
+        </button>
+        <div>
+          <Link to="/about">👤 {loggedInUser}</Link>
+        </div>
       </div>
     </header>
   );
